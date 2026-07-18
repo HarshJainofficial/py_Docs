45a7f013ae4aa2941cdb15897c7306e367642cbd
Harsh Jain <hjainofficial4@gmail.com>
2026-07-18 18:52:06 +0530

    Initial Python Doc

---
+++ python.py
@@ -0,0 +1,79 @@
+print("Hello Python! Practise 2.0")
+
+# Print Area of Rectangle
+
+# length = 5 
+# width = 2 
+# area = length * width
+# print("Area of Rectangle is",area)
+
+
+# celsius to fahrenheit conversion
+
+# celsius = int(input("number is "))
+# fahrenheit = (celsius*1.8) + 32
+# print(fahrenheit)
+
+
+# Boolean Input question 
+# There is no direct boolean input. Convert it yourself.
+# answer = input("True or False ").lower()
+# is_valid = answer == "true"
+# print(is_valid)
+
+# Multiple Inputs ( Same Line )
+# a , b , c = input("Enter three numbers ").split()
+# print("Sum of a + b + c =", int(a) + int(b) + int(c));
+# print(int(a)+int(b)+int(c))
+# print(a,"",b,"",c)
+
+# Multiple int input
+# a,b = map(int, input("enter the numbers ").split())
+# print(a + b)
+
+# Simple Interest (  calculate by the multiple float input )
+# Simple interest formula PXRXT/100
+# p , r , t = map(float, input("Enter the principal, rate, and time ").split())
+# si = (p*r*t)/100
+# print(si)
+# print(int(si))
+
+
+# Beginner Alternative (Without using map function)(Multiple Integer input without map function)
+# values = input("Enter the principle,rate and time").split()
+# p = float(values[0])
+# q = float(values[1])
+# r = float(values[2])
+# print(float(p*q*r)/100)
+
+
+
+# Function in Py : Simple interest calculate by function ( Problem)
+
+# def simple_interest(p,r,t):
+#     si = (p*r*t)/100
+#     return si 
+
+# Single line input as float
+# p = float(input("Enter the principle :")) 
+# q = float(input("Enter the rate :"))
+# r = float(input("Enter the time(in years): "))
+
+# Multiple line input as float 
+# p,q,r = map(float,input("Enter the principle , rate and time").split())
+
+# interest = simple_interest(p,q,r)
+# print(interest)
+
+
+# 8. List of Integers ⭐ (Very Important for DSA)
+# numbers = list(map(int,input("Enter the numbers :").split()))
+# print(numbers)
+
+# 9. List of Strings ⭐(Very Important for Basic of input in Pytho )
+# fruits = input().split()
+# print(fruits)
+
+# Tuple Input
+
+
